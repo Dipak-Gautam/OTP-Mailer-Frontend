51d70723ece536d8f01c7884560131c7a948c421
Dipak-Gautam <anjangautam095@gmail.com>
2025-04-13 15:02:39 +0545

    feat: router and custom font setup

--- src/Components/HeroImage/HeroImage.tsx
+++ src/Components/HeroImage/HeroImage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HeroImage = () => {
-  return <div>HEllo from hero image</div>;
+  return <div className="font-SpaceMono">HEllo from hero image</div>;
 };
 
 export default HeroImage;
