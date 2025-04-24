5879fa148469a921c6e03275fdc8987ef91c1597
Dipak-Gautam <anjangautam095@gmail.com>
2025-04-24 13:19:55 +0545

    feat : delete account implemented

--- src/Components/DashBoard/Component/AccountApi/Account&Api.tsx
+++ src/Components/DashBoard/Component/AccountApi/Account&Api.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import ApiKey from "./Component/ApiKey";
 import AccountSetting from "./Component/AccountSetting";
+import DeleteAccount from "./Component/DeleteAccount";
 
 const AccountApi = () => {
   return (
-    <div className="p-5">
+    <div className="p-5 h-[100vh] overflow-auto">
       <div className="text-4xl font-SpaceMono italic font-bold ">
         Account Management
       </div>
-      <div className="my-7 space-y-10">
+      <div className=" flex-1 my-7 space-y-10 ">
         <ApiKey />
         <AccountSetting />
+        <DeleteAccount />
       </div>
     </div>
   );
---
+++ src/Components/DashBoard/Component/AccountApi/Component/DeleteAccount.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const DeleteAccount = () => {
+  return (
+    <div className="border p-4 rounded-xl border-gray-300 shadow-xl shadow-gray-400/50">
+      <div className="text-xl font-medium text-gray-900">Delete Account</div>
+      <div className="flex flex-1">
+        <div className="flex flex-1 justify-center items-center">
+          <div className="p-2 px-3 bg-red-500 text-white font-semibold rounded-md cursor-pointer hover:bg-red-600 transition duration-200 ease-in-out">
+            Delete Account
+          </div>
+        </div>
+        <div className="w-[40%] space-y-4">
+          <div className="font-SpaceMono font-bold text-2xl italic text-center">
+            Delete Account
+          </div>
+          <div className="text-gray-500 font-medium text-lg">
+            This will permanently remove your virtual assistance account. You
+            will not be able to restore your account after you delete it.
+          </div>
+          <div className="text-red-500 italic  text-lg">
+            <p>Note :</p>
+            Warning! This action cannot be undone. This will permanently delete
+            your account.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteAccount;
