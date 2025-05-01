8b0d982e797f0828d3f1e476d2286321c083d76a
Dipak-Gautam <anjangautam095@gmail.com>
2025-05-01 18:31:31 +0545

    chore: test email started

--- src/Components/DashBoard/Component/Dashboard/SaveAndTest/SaveAndTest.tsx
+++ src/Components/DashBoard/Component/Dashboard/SaveAndTest/SaveAndTest.tsx
@@ -1,11 +1,13 @@
 import OrangeButton from "../../../../Buttons/OrangeButton/OrangeButton";
 import UpdateButton from "../../../../Buttons/UpdateButton/UpdateButton";
+import SaveModel from "./SaveModel";
 
 const SaveAndTest = () => {
   return (
     <div className="flex justify-center items-center gap-8">
       <OrangeButton title="Save" />
       <UpdateButton title="Send Test Mail" />
+      <SaveModel />
     </div>
   );
 };
---
+++ src/Components/DashBoard/Component/Dashboard/SaveAndTest/SaveModel.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SaveModel = () => {
+  return (
+    <div className="fixed inset-0 flex justify-center items-center bg-slate-500/50 transition-colors ">
+      Hello
+    </div>
+  );
+};
+
+export default SaveModel;
