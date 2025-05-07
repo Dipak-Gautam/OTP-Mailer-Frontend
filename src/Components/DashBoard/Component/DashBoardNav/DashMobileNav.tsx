a14dfb8b46e84d22e7e25ac1c2467a16182136fb
Dipak-Gautam <anjangautam095@gmail.com>
2025-05-07 20:55:33 +0545

    chore: dashboard navigation bug fixes

--- src/Components/DashBoard/Component/DashBoardNav/DashMobileNav.tsx
+++ src/Components/DashBoard/Component/DashBoardNav/DashMobileNav.tsx
@@ -2,16 +2,21 @@ import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { TbNorthStar } from "react-icons/tb";
 import MobileDashNav from "./Components/MobileDashNav";
+import { useNavigate } from "react-router-dom";
 
 const DashMobileNav = () => {
   const [show, setShow] = useState(true);
+  const navigate = useNavigate();
   return (
     <div className="md:hidden bg-[#f3e5dc] p-5 justify-between flex">
-      <div className="flex justify-center items-center gap-4 ">
+      <div
+        className="flex justify-center items-center gap-2 "
+        onClick={() => navigate("/dashboard")}
+      >
         <div className="border border-gray-700  rounded-full p-2 bg-gray-700 cursor-pointer w-fit">
           <TbNorthStar className="text-white text-2xl" />
         </div>
-        <div className="text-lg font-SpaceMono text-black italic font-bold">
+        <div className="text-lg font-SpaceMono text-black italic font-bold cursor-pointer">
           OTP-Mailer:
         </div>
       </div>
