4e75723881728bc25e54d3b865e6da8788b21d6d
Dipak-Gautam <anjangautam095@gmail.com>
2026-01-18 14:23:11 +0545

    feat: welcome template setup

--- src/Components/DashBoard/Component/WelcomeMail/Component/WelcomeTemplate.tsx
+++ src/Components/DashBoard/Component/WelcomeMail/Component/WelcomeTemplate.tsx
@@ -1,7 +1,44 @@
 import React from "react";
 
 const WelcomeTemplate = () => {
-  return <div>hello from welcome template</div>;
+  return (
+    <div className="">
+      <div className="bg-gray-100 text-gray-800 font-sans">
+        <div className="max-w-[600px] mx-auto  bg-white rounded-lg shadow-lg overflow-hidden border border-gray-300 text-sm">
+          <div className="bg-blue-600 text-white text-center text-2xl font-bold p-3">
+            Welcome to Our Community!
+          </div>
+
+          <div className="p-4 leading-relaxed">
+            <p className="">Hello Dipak,</p>
+
+            <p className="mb-2">
+              We’re thrilled to have you join us! Your registration was
+              successful, and we’re committed to providing you with the best
+              experience possible.
+            </p>
+
+            <p className="mb-2">Here’s how you can get started:</p>
+
+            <p className="inline-block bg-blue-600 text-white font-bold text-sm px-6 py-2 rounded-md transition-colors hover:bg-blue-700 cursor-pointer">
+              Get Started
+            </p>
+
+            <p className="mt-3">
+              If you need any help, don’t hesitate to contact us. We’re here to
+              support you every step of the way.
+            </p>
+          </div>
+
+          <div className="bg-gray-100 text-center text-gray-500 text-xs p-3 border-t border-gray-300">
+            <p>
+              &copy; ${new Date().getFullYear()} OtpMailer. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WelcomeTemplate;
--- src/Components/DashBoard/Component/WelcomeMail/WelcomeMail.tsx
+++ src/Components/DashBoard/Component/WelcomeMail/WelcomeMail.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import WelcomeTemplate from "./Component/WelcomeTemplate";
 
 const WelcomeMail = () => {
-  return <div>Hello from welcome mail</div>;
+  const [subject, setSubject] = useState("Welcome Mail");
+  return (
+    <div className="flex gap-2 h-full p-3">
+      <div className="flex flex-col flex-1 px-5 gap-3">
+        <div className="font-SpaceMono text-xl italic font-bold text-center mb-5">
+          Configure the welcome email
+        </div>
+        <div>
+          <div className="flex-1 relative">
+            <span className="absolute top-0 text-[11px] text-gray-600 font-medium bg-white px-1 left-2 -translate-y-2">
+              Subject
+            </span>
+            <input
+              value={subject}
+              onChange={(event) => {
+                setSubject(event.target.value);
+              }}
+              className="border border-gray-400 rounded-md p-2 text-base px-3 w-full outline-none"
+              placeholder="Subject"
+            />
+          </div>
+        </div>
+
+        <WelcomeTemplate />
+      </div>
+      <div className="flex border-l border-black flex-1 bg-slate-50"></div>
+    </div>
+  );
 };
 
 export default WelcomeMail;
